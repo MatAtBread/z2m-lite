34803149f22eb373b0fe48dfa5c7e9ed53b6279c
Matt <bread@mailed.me.uk>
2025-04-25 22:22:30 +0100

    Fix lower case

--- rules/boiler.js
+++ rules/boiler.js
@@ -14,6 +14,7 @@ function isTrv(value) {
         && 'system_mode' in value && 'position' in value
 }
 
+let lastUpdate = '';
 // Prevent recursive calls to this rule
 if (!update.includes('/set') && isTrv(state[update])) {
     const trvs = Object.entries(state).filter(([key, value]) => isTrv(value));
@@ -21,7 +22,7 @@ if (!update.includes('/set') && isTrv(state[update])) {
     const positions = trvs.map(([name,state]) => state.position).filter(pos => typeof pos === 'number' && !isNaN(pos));
     const position = Math.max(...positions);
     const atTemp = trvs
-        .filter(([name,state]) => state.system_mode === 'heat' || state.system_mode === 'auto')
+        .filter(([name,state]) => state.system_mode.toLowerCase() === 'heat' || state.system_mode.toLowerCase() === 'auto')
         .map(([name,state]) => typeof state.local_temperature !== 'number'
             || typeof state.current_heating_setpoint !== 'number'
             || state.local_temperature >= state.current_heating_setpoint);
@@ -35,9 +36,11 @@ if (!update.includes('/set') && isTrv(state[update])) {
         desired = 'ON';
     }
 
+
     const current = state["zigbee2mqtt/Central Heating"]?.state_l3;
-    if (desired && current && desired !== current) {
-        publish('zigbee2mqtt/Central Heating/set', { "state_l3": desired });
+    console.log("TRVs: ", trvs.map(([name]) => name), "\nposition: ", position, "\natTemp: ", atTemp, "\ndesired: ", desired, "\ncurrent: ", current);
+    if (desired && current && desired !== current /*&& desired !== lastUpdate*/) {
+        publish('zigbee2mqtt/Central Heating/set', { "state_l3": lastUpdate = desired });
     }
 }
 
